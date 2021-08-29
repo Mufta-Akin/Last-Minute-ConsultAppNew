e81bc97cae5b3f5d51046c99eaf5140a46bc5229
Muyiwa Akin-Fatoki <71923975+Mufta-Akin@users.noreply.github.com>
2021-08-29 17:52:04 +0100

    Diagnosis Controller and Index

--- Consultation.Web/Controllers/DiagnosisController.cs
+++ Consultation.Web/Controllers/DiagnosisController.cs
@@ -7,160 +7,24 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Consultation.Data.Models;
 using Consultation.Data.Repositories;
+using Consultation.Data.Services;
 
 namespace Consultation.Web.Controllers
 {
-    public class DiagnosisController : Controller
+    public class DiagnosisController : BaseController
     {
-        private readonly DatabaseContext _context;
+        private readonly PracticeService _svc;
 
-        public DiagnosisController(DatabaseContext context)
+        public DiagnosisController(PracticeService svc)
         {
-            _context = context;
+            _svc = svc;
         }
 
-        // GET: Diagnosis
-        public async Task<IActionResult> Index()
+        public IActionResult DiagnosisIndex()
         {
-            var databaseContext = _context.Diagnoses.Include(d => d.Doctor).Include(d => d.Patient);
-            return View(await databaseContext.ToListAsync());
-        }
-
-        // GET: Diagnosis/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var diagnosis = await _context.Diagnoses
-                .Include(d => d.Doctor)
-                .Include(d => d.Patient)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (diagnosis == null)
-            {
-                return NotFound();
-            }
-
-            return View(diagnosis);
-        }
-
-        // GET: Diagnosis/Create
-        public IActionResult Create()
-        {
-            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Mobile");
-            ViewData["PatientId"] = new SelectList(_context.Doctors, "Id", "Mobile");
+            // dashboard for Ailment   
             return View();
         }
-
-        // POST: Diagnosis/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,DiagnosedOn,ConfirmedOn,DoctorId,PatientId,Description")] Diagnosis diagnosis)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(diagnosis);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Mobile", diagnosis.DoctorId);
-            ViewData["PatientId"] = new SelectList(_context.Doctors, "Id", "Mobile", diagnosis.PatientId);
-            return View(diagnosis);
-        }
-
-        // GET: Diagnosis/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var diagnosis = await _context.Diagnoses.FindAsync(id);
-            if (diagnosis == null)
-            {
-                return NotFound();
-            }
-            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Mobile", diagnosis.DoctorId);
-            ViewData["PatientId"] = new SelectList(_context.Doctors, "Id", "Mobile", diagnosis.PatientId);
-            return View(diagnosis);
-        }
-
-        // POST: Diagnosis/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,DiagnosedOn,ConfirmedOn,DoctorId,PatientId,Description")] Diagnosis diagnosis)
-        {
-            if (id != diagnosis.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(diagnosis);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!DiagnosisExists(diagnosis.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Mobile", diagnosis.DoctorId);
-            ViewData["PatientId"] = new SelectList(_context.Doctors, "Id", "Mobile", diagnosis.PatientId);
-            return View(diagnosis);
-        }
-
-        // GET: Diagnosis/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var diagnosis = await _context.Diagnoses
-                .Include(d => d.Doctor)
-                .Include(d => d.Patient)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (diagnosis == null)
-            {
-                return NotFound();
-            }
-
-            return View(diagnosis);
-        }
-
-        // POST: Diagnosis/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var diagnosis = await _context.Diagnoses.FindAsync(id);
-            _context.Diagnoses.Remove(diagnosis);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool DiagnosisExists(int id)
-        {
-            return _context.Diagnoses.Any(e => e.Id == id);
-        }
+                    
     }
 }
