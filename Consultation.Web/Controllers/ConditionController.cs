d6a1b9217974eb4071b28e1733758a2ebf2ee227
Muyiwa Akin-Fatoki <71923975+Mufta-Akin@users.noreply.github.com>
2021-08-29 11:32:14 +0100

    Condition-Diagnosis Controllers & Views

---
+++ Consultation.Web/Controllers/ConditionController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Consultation.Data.Models;
+using Consultation.Data.Repositories;
+
+namespace Consultation.Web.Controllers
+{
+    public class ConditionController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public ConditionController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Condition
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Conditions.ToListAsync());
+        }
+
+        // GET: Condition/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var condition = await _context.Conditions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (condition == null)
+            {
+                return NotFound();
+            }
+
+            return View(condition);
+        }
+
+        // GET: Condition/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Condition/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,DiagnosedDate,Description")] Condition condition)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(condition);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(condition);
+        }
+
+        // GET: Condition/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var condition = await _context.Conditions.FindAsync(id);
+            if (condition == null)
+            {
+                return NotFound();
+            }
+            return View(condition);
+        }
+
+        // POST: Condition/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,DiagnosedDate,Description")] Condition condition)
+        {
+            if (id != condition.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(condition);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConditionExists(condition.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(condition);
+        }
+
+        // GET: Condition/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var condition = await _context.Conditions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (condition == null)
+            {
+                return NotFound();
+            }
+
+            return View(condition);
+        }
+
+        // POST: Condition/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var condition = await _context.Conditions.FindAsync(id);
+            _context.Conditions.Remove(condition);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConditionExists(int id)
+        {
+            return _context.Conditions.Any(e => e.Id == id);
+        }
+    }
+}
